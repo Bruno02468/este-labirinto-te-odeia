487ae12634a155c3bcfe4afabf204916dea19949
Bruno Borges Paschoalinoto <bruno@paschoalinoto.com>
2025-06-23 15:26:40 -0300

    Finished Menu + Custom runs options

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -2,14 +2,21 @@ extends Node2D
 
 @onready var titleText = $Title
 @onready var diffs = $Dificults
+@onready var custom = $CustomSelect
+
+@onready var customGo = $CustomSelect/CustomGo
+@onready var customRooms = $CustomSelect/Rooms
+@onready var customPortals = $CustomSelect/Portals
+@onready var customTime = $CustomSelect/Time
 
 var textUnshuffled : String = "Este Labirinto te Odeia"
 var shuffleNow : bool = true
 
-
 func _ready() -> void:
-	
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	
+	custom.visible = false
+	diffs.visible = true
 	
 	Global.N_Rooms = 4
 	Global.N_PortalsPerRoom = 4
@@ -35,26 +42,31 @@ func _on_timer_timeout() -> void:
 	$Title.text = "[center][shake rate=20.0 level=5 connected=1]%s[/shake][/center]" % textInsert
 
 func _on_easy_pressed() -> void:
-	print("Easy: \n %d Rooms Time: %f",Global.N_Rooms,Global.targetTime)
 	Global.N_Rooms = 4
 	Global.targetTime = 120.0
+	print("Easy: \n  ",Global.N_Rooms, " Rooms Time: ",Global.targetTime,"s")
 	get_node("/root/Main").load_scene("res://scenes/labirint_test.tscn")
 
 func _on_medium_pressed() -> void:
-	print("Medium: \n %d Rooms Time: %f",Global.N_Rooms,Global.targetTime)
 	Global.N_Rooms = 8
 	Global.targetTime = 60.0
+	print("Medium: \n  ",Global.N_Rooms, " Rooms Time: ",Global.targetTime,"s")
 	get_node("/root/Main").load_scene("res://scenes/labirint_test.tscn")
 
 func _on_hard_pressed() -> void:
-	print("Hard: \n %d Rooms Time: %f",Global.N_Rooms,Global.targetTime)
 	Global.N_Rooms = 16
 	Global.targetTime = 30.0
+	print("Hard: \n  ",Global.N_Rooms, " Rooms Time: ",Global.targetTime,"s")
 	get_node("/root/Main").load_scene("res://scenes/labirint_test.tscn")
 
 func _on_custom_pressed() -> void:
 	diffs.visible= false
-	Global.N_Rooms = 4
-	Global.N_PortalsPerRoom = 4
-	Global.targetTime = 120.0
-	print("Foi custom")
+	custom.visible = true
+	
+	await customGo.pressed
+	
+	Global.N_Rooms = customRooms.value
+	Global.N_PortalsPerRoom = customPortals.value
+	Global.targetTime = customTime.value
+	print("Custom: \n  ",Global.N_Rooms, " Rooms ",Global.N_PortalsPerRoom," Portals Time: ",Global.targetTime,"s")
+	get_node("/root/Main").load_scene("res://scenes/labirint_test.tscn")
